0ee21323d055a91da66b3231ba4b99cb1367601a
Georgio El-Khoury <99497326+Georgio-Khoury@users.noreply.github.com>
2024-06-21 02:15:06 +0300

    Update USER.sql

--- USER.sql
+++ USER.sql
@@ -1,10 +1,14 @@
 CREATE TABLE "users" (
   "userid" integer PRIMARY KEY,
+  "password" varchar(255),
   "firstname" varchar(255),
   "lastname" varchar(255),
   "phonenb" varchar(20),
   "experience_level" varchar(255),
+  "education" varchar(255),
+  "description" varchar(255),
   "job_role" varchar(255),
   "joindate" timestamp,
-  "profile_picture" BYTEA
+  "profile_picture" BYTEA,
+  "admin" boolean
 );
