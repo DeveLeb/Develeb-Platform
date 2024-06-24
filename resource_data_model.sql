835ecdf5b8e3d71b470eab50a1daa252eb1e317a
faridaalamine2 <130583314+faridaalamine2@users.noreply.github.com>
2024-06-24 22:34:14 +0300

    resolve issues in data model

--- resource_data_model.sql
+++ resource_data_model.sql
@@ -1,20 +1,34 @@
-CREATE TABLE "resources" (
-  "resourceId" integer PRIMARY KEY,
+CREATE TABLE "resource" (
+  "id" integer PRIMARY KEY,
   "title" varchar(255) NOT NULL,
-  "tags" varchar(255)[],
   "description" TEXT,
-  "link" varchar(1000) NOT NULL,
+  "link" varchar(255) NOT NULL,
   "publish" boolean,
-  "type" varchar(100),
+  "type" varchar(255),
   "created_at" timestamp NOT NULL DEFAULT (now()),
-  "updated_at" timestamp 
+  "updated_at" timestamp
+);
+
+CREATE TABLE "tag" (
+  "id" integer PRIMARY KEY,
+  "tag_name" varchar(255)
+);
+
+CREATE TABLE "resource_tags" (
+  "resource_id" integer NOT NULL,
+  "tag_id" integer NOT NULL,
+  PRIMARY KEY ("resource_id", "tag_id")
 );
 
 CREATE TABLE "resource_views" (
-  "viewId" integer PRIMARY KEY,
-  "resourceId" integer NOT NULL,
-  "view_count" integer DEFAULT 0,
-  "last_viewed_date" timestamp DEFAULT (now())
+  "user_id" integer PRIMARY KEY,
+  "resource_id" integer,
+  "total_views" integer,
+  "last_viewed_at" timestamp DEFAULT (now())
 );
 
-ALTER TABLE "resource_views" ADD FOREIGN KEY ("resourceId") REFERENCES "resources" ("resourceId");
+ALTER TABLE "resource_tags" ADD FOREIGN KEY ("resource_id") REFERENCES "resource" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "resource_tags" ADD FOREIGN KEY ("tag_id") REFERENCES "tag" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "resource_views" ADD FOREIGN KEY ("resource_id") REFERENCES "resource" ("id");
