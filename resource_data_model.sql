0b77c7a9df5ed42e9ea5aa652a69f7954b62369c
faridaalamine2 <130583314+faridaalamine2@users.noreply.github.com>
2024-06-25 22:56:46 +0300

    Update resource_data_model.sql

--- resource_data_model.sql
+++ resource_data_model.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "resource" (
   "id" integer PRIMARY KEY,
   "title" varchar(255) NOT NULL,
+  "tag_ids" integer[],
   "description" TEXT,
   "link" varchar(255) NOT NULL,
   "publish" boolean,
@@ -14,21 +15,11 @@ CREATE TABLE "tag" (
   "tag_name" varchar(255)
 );
 
-CREATE TABLE "resource_tags" (
-  "resource_id" integer NOT NULL,
-  "tag_id" integer NOT NULL,
-  PRIMARY KEY ("resource_id", "tag_id")
-);
-
-CREATE TABLE "resource_views" (
-  "user_id" integer PRIMARY KEY,
+CREATE TABLE "resource_view" (
+  "id" integer PRIMARY KEY,
+  "user_id" integer,
   "resource_id" integer,
-  "total_views" integer,
-  "last_viewed_at" timestamp DEFAULT (now())
+  "viewed_at" timestamp DEFAULT (now())
 );
 
-ALTER TABLE "resource_tags" ADD FOREIGN KEY ("resource_id") REFERENCES "resource" ("id") ON DELETE CASCADE;
-
-ALTER TABLE "resource_tags" ADD FOREIGN KEY ("tag_id") REFERENCES "tag" ("id") ON DELETE CASCADE;
-
-ALTER TABLE "resource_views" ADD FOREIGN KEY ("resource_id") REFERENCES "resource" ("id");
+ALTER TABLE "resource_view" ADD FOREIGN KEY ("resource_id") REFERENCES "resource" ("id");
