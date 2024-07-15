aa67d9427ad56be74d3de49fe9b77af1c0ec4965
Tony <tonyayda7@gmail.com>
2024-07-15 22:32:58 +0300

    Add files via upload

--- packages/backend/drizzle.config.ts
+++ packages/backend/drizzle.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: 'postgresql://postgres:letmein@localhost:5432/Develeb-platform',
   },
   verbose: true,
   strict: true,
