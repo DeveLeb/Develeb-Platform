a2923c65bb5af989f00fc8523aaddbc17b016e9d
Tony <tonyayda7@gmail.com>
2024-07-15 21:27:44 +0300

    Update drizzle.config.ts

--- packages/backend/drizzle.config.ts
+++ packages/backend/drizzle.config.ts
@@ -3,8 +3,8 @@ dotenv.config();
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/drizzle/schema.ts',
-  out: './src/drizzle/migrations',
+  schema: './src/db/schema.ts',
+  out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL,
