09b41213a77ba23b771d03ea61b632cd3c6535d2
ren herbawy <202111979@ua.edu.lb>
2024-07-16 19:03:34 +0300

    event search panel

--- packages/frontend/src/components/molecules/EventSearchPanel.tsx
+++ packages/frontend/src/components/molecules/EventSearchPanel.tsx
@@ -1,6 +1,6 @@
-// src/components/molecules/EventSearchPanel.tsx
 import React from 'react';
-import { Button } from "@/components/ui/button";
+
+import { Button } from '@/components/ui/button';
 
 interface SearchPanelProps {
   onSearch: () => void;
@@ -10,13 +10,10 @@ const EventSearchPanel: React.FC<SearchPanelProps> = ({ onSearch }) => {
   return (
     <div className="w-[2000px] h-[385px] bg-gray-300 p-10 shadow-lg flex justify-center">
       <div className="max-w-[1400px] w-full flex flex-col justify-center">
-        <h1 className="text-[40px] font-bold text-white mb-3 text-left">
-          Checkout Our Latest Events
-        </h1>
+        <h1 className="text-[40px] font-bold text-white mb-3 text-left">Checkout Our Latest Events</h1>
         <p className="text-[20px] text-white mb-6 text-left">
           Lorem ipsum dolor sit amet consectetur. Ultrices sem est nunc massa.
         </p>
-        
         <div className="flex items-end">
           <div className="flex flex-col mb-4">
             <input
@@ -36,10 +33,7 @@ const EventSearchPanel: React.FC<SearchPanelProps> = ({ onSearch }) => {
             />
           </div>
           
-          <Button
-            onClick={onSearch}
-            className="h-[60px] bg-gray-800 hover:bg-gray-600 text-white font-bold mb-4"
-          >
+          <Button onClick={onSearch} className="h-[60px] bg-gray-800 hover:bg-gray-600 text-white font-bold mb-4">
             Search Event
           </Button>
         </div>
