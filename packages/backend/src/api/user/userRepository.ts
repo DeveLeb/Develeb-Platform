19b99afdb800c34e759704fe73ac1beef83b7205
Georgio Khoury <georgiok033@hotmail.com>
2024-07-21 13:38:48 +0300

    removed commented obj

--- packages/backend/src/api/user/userRepository.ts
+++ packages/backend/src/api/user/userRepository.ts
@@ -5,42 +5,6 @@ import { db } from 'src/db';
 import { user } from 'src/db/schema';
 
 import { User } from '../user/userModel';
-// export const users: User[] = [
-//   {
-//     id: '3',
-//     fullname: 'Charlie',
-//     email: 'charlie@example.com',
-//     password: '654321',
-//     tags: 'developer',
-//     is_verified: true,
-//     created_at: new Date(),
-//     updated_at: new Date(),
-//     role: 'user',
-//     username: 'charlie321',
-//     profile_url:
-//       'https://cloudinary-marketing-res.cloudinary.com/images/w_1000,c_scale/v1679921049/Image_URL_header/Image_URL_header-png?_i=AA',
-//     category_id: 2,
-//     level_id: 1,
-//     phone_number: '+961 12345678',
-//   },
-//   {
-//     id: '2',
-//     fullname: 'Bob',
-//     email: 'bob@example.com',
-//     password: '1233456',
-//     created_at: new Date(),
-//     updated_at: new Date(),
-//     role: 'admin',
-//     username: 'bob123',
-//     profile_url:
-//       'https://cloudinary-marketing-res.cloudinary.com/images/w_1000,c_scale/v1679921049/Image_URL_header/Image_URL_header-png?_i=AA',
-//     is_verified: false,
-//     category_id: 1,
-//     level_id: 1,
-//     phone_number: '+961 54738292',
-//     tags: 'developer',
-//   },
-// ];
 
 export const userRepository = {
   findAllAsync: async () => {
