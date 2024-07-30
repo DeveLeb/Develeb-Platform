45a0e75e9cf967a5f55cc5e0ce19efc7f0397e36
Tony <tonyayda7@gmail.com>
2024-07-30 13:09:57 +0300

    add resourcesRoutes

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -5,6 +5,7 @@ import { pino } from 'pino';
 
 import { healthCheckRouter } from './api/healthCheck/healthCheckRouter';
 import { jobRouter } from './api/job/jobRouter';
+import { resourceRouter } from './api/resources/resourceRouter';
 import { userRouter } from './api/user/userRouter';
 import { openAPIRouter } from './api-docs/openAPIRouter';
 import errorHandler from './common/middleware/errorHandler';
@@ -31,6 +32,7 @@ app.use(requestLogger);
 app.use('/health-check', healthCheckRouter);
 app.use('/users', userRouter);
 app.use('/jobs', jobRouter);
+app.use('/resources', resourceRouter);
 
 // Swagger UI
 app.use(openAPIRouter);
