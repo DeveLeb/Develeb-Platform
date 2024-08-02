6cfc1d84aac23591f17768af9608a4dec0d4a441
Georgio <georgiok033@hotmail.com>
2024-08-02 14:24:34 +0300

    minor ref

--- packages/backend/src/common/middleware/authConfig/authorizeRole.ts
+++ packages/backend/src/common/middleware/authConfig/authorizeRole.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
-import { StatusCodes, UNAUTHORIZED } from 'http-status-codes';
+import { StatusCodes } from 'http-status-codes';
 import { User } from 'src/api/user/userModel';
 import { ResponseStatus, ServiceResponse } from 'src/common/models/serviceResponse';
 import { logger } from 'src/server';
 
 import { Roles } from './roles';
+import { handleServiceResponse } from 'src/common/utils/httpHandlers';
 
 const authorizeRole = (role: Roles) => {
   return (req: Request, res: Response, next: NextFunction) => {
@@ -13,23 +14,27 @@ const authorizeRole = (role: Roles) => {
     logger.info('Checking if user is logged in');
     if (!currentUser) {
       logger.error('User is not logged in');
-      return res
-        .status(StatusCodes.UNAUTHORIZED)
-        .json(
-          new ServiceResponse(
-            ResponseStatus.Failed,
-            'Unauthorized',
-            { error: 'Unauthorized' },
-            StatusCodes.UNAUTHORIZED
-          )
-        );
+      const serviceResponse = new ServiceResponse(
+        ResponseStatus.Failed,
+        'Unauthorized',
+        { error: 'User is unauthorized' },
+        StatusCodes.UNAUTHORIZED
+      );
+      return handleServiceResponse(serviceResponse, res);
     }
     logger.info('User logged in , checking if user has the correct role');
     if (currentUser.role !== role) {
       logger.info('User does not have the required role');
-      return res
-        .status(StatusCodes.FORBIDDEN)
-        .json(new ServiceResponse(ResponseStatus.Failed, 'Forbidden', { error: 'Forbidden' }, StatusCodes.FORBIDDEN));
+      // return res
+      //   .status(StatusCodes.FORBIDDEN)
+      //   .json(new ServiceResponse(ResponseStatus.Failed, 'Forbidden', { error: 'Forbidden' }, StatusCodes.FORBIDDEN));
+      const serviceResponse = new ServiceResponse(
+        ResponseStatus.Failed,
+        'Forbidden',
+        { error: 'Forbidden' },
+        StatusCodes.FORBIDDEN
+      );
+      return handleServiceResponse(serviceResponse, res);
     }
     logger.info('User authorized');
     next();
