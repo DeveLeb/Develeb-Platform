612f76a06a8d0fecac989fb67647810565ac1704
Georgio Khoury <georgiok033@hotmail.com>
2024-08-10 17:56:28 +0300

    minor problems fixes

--- packages/backend/src/api/user/userResponse.ts
+++ packages/backend/src/api/user/userResponse.ts
@@ -1,8 +1,10 @@
+import { Roles } from 'src/common/middleware/authConfig/roles';
 export interface UserResponse {
   id: string;
   email: string;
   username: string;
   password: string;
+  role: Roles;
   fullName: string;
   phoneNumber: string;
   levelId: number;
