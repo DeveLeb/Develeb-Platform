96c307a16bc066450a66f1e433bc6cb7f87a6595
Hkataya <Hkataya@users.noreply.github.com>
2024-09-12 18:37:05 +0200

    Cleanup and setup files for deployment

--- packages/backend/tsup.config.ts
+++ packages/backend/tsup.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['./src'],
+  format: ['cjs', 'esm'],
+  outDir: 'dist',
+  minify: true,
   splitting: false,
   sourcemap: true,
   clean: true,
+  loader: {
+    '.sql': 'file',
+  },
 });
