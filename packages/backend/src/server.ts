ea64fb6eb2a02b44e51c0f586c1f6e62595ccbe9
the-refrigerator <65685646+the-refrigerator@users.noreply.github.com>
2024-09-17 18:57:51 +0300

    refactor(cicd): moved pino options to `common/utils/loggerOptions.ts`

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -15,33 +15,7 @@ import errorHandler from './common/middleware/errorHandler';
 import rateLimiter from './common/middleware/rateLimiter';
 import requestLogger from './common/middleware/requestLogger';
 import { env } from './common/utils/envConfig';
-
-const loggerOptions = [{ level: 'info' }];
-
-if (process.env.isProduction || process.env.NODE_ENV === 'production') {
-  if (process.env.LOKI_HOST && process.env.LOKI_USERNAME && process.env.LOKI_PASSWORD) {
-    loggerOptions.push(
-      pino.transport({
-        target: 'pino-loki',
-        options: {
-          batching: true,
-          interval: 5,
-          labels: { application: 'production-server' },
-
-          host: process.env.LOKI_HOST,
-          basicAuth: {
-            username: process.env.LOKI_USERNAME,
-            password: process.env.LOKI_PASSWORD,
-          },
-        },
-      })
-    );
-  } else {
-    console.error(
-      'Loki logging is enabled but missing required environment variables. Please set LOKI_HOST, LOKI_USERNAME, and LOKI_PASSWORD. Defaulting to console logging.'
-    );
-  }
-}
+import { loggerOptions } from './common/utils/loggerOptions';
 
 const logger = pino(...loggerOptions);
 
